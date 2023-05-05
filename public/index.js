505b224d8d17519e43d2b1c4e38dd9b72d184917
prime-infinity <soe.a.prime@gmail.com>
2023-05-05 08:31:48 -0700

    did force directed graph on users

--- public/index.js
+++ public/index.js
@@ -1,26 +1,79 @@
-// Create a dataset
-var dataset = [5, 10, 15, 20, 25];
+import { users } from "./users2.js";
+//console.log(users);
+// Select a random user
+const mainUser = users[Math.floor(Math.random() * users.length)];
 
-// Create an SVG element
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", 500)
-  .attr("height", 300);
+// Create an array of nodes with the main user and his followers
+const nodes = [mainUser];
+mainUser.followers.forEach((followerId) => {
+  const follower = users.find((user) => user.id === followerId);
+  if (follower) nodes.push(follower);
+});
 
-// Create bars
-svg
-  .selectAll("rect")
-  .data(dataset)
-  .enter()
-  .append("rect")
-  .attr("x", function (d, i) {
-    return i * 50;
-  })
-  .attr("y", function (d) {
-    return 300 - d * 10;
-  })
-  .attr("width", 40)
-  .attr("height", function (d) {
-    return d * 10;
-  });
+// Create an array of links between the main user and his followers
+const links = mainUser.followers.map((followerId) => ({
+  source: mainUser.id,
+  target: followerId,
+}));
+
+// Set up the D3 force simulation
+const simulation = d3
+  .forceSimulation(nodes)
+  .force(
+    "link",
+    d3.forceLink(links).id((d) => d.id)
+  )
+  .force("charge", d3.forceManyBody().strength(-100))
+  .force(
+    "center",
+    d3.forceCenter(window.innerWidth / 2, window.innerHeight / 2)
+  );
+
+// Create SVG elements for the nodes and links
+const svg = d3.select("svg");
+const link = svg
+  .append("g")
+  .attr("stroke", "#999")
+  .attr("stroke-opacity", 0.6)
+  .selectAll("line")
+  .data(links)
+  .join("line")
+  .attr("stroke-width", (d) => Math.sqrt(d.value));
+const node = svg
+  .append("g")
+  .attr("stroke", "#fff")
+  .attr("stroke-width", 1.5)
+  .selectAll("circle")
+  .data(nodes)
+  .join("circle")
+  .attr("r", 5)
+  .attr("fill", (d) => (d === mainUser ? "red" : "blue"))
+  .call(
+    d3
+      .drag()
+      .on("start", (event, d) => {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      })
+      .on("drag", (event, d) => {
+        d.fx = event.x;
+        d.fy = event.y;
+      })
+      .on("end", (event, d) => {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      })
+  );
+
+// Run the simulation and update the positions of the nodes and links
+simulation.on("tick", () => {
+  link
+    .attr("x1", (d) => d.source.x)
+    .attr("y1", (d) => d.source.y)
+    .attr("x2", (d) => d.target.x)
+    .attr("y2", (d) => d.target.y);
+
+  node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+});
