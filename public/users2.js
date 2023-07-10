aed447bc3e13d0f1c5405001877c160baa10298d
prime-infinity <soe.a.prime@gmail.com>
2023-07-10 08:51:22 -0700

    new init

--- public/users2.js
+++ public/users2.js
@@ -1,7 +1,7 @@
 /**
  * note that the app becomes slower the more the followers
  */
-const MAX_USERS = 50;
+const MAX_USERS = 1000;
 // Generate an array of user objects
 export const users = Array.from({ length: MAX_USERS }, (_, i) => ({
   id: i + 1,
